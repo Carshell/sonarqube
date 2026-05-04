2a65567c26796b66e70d08e59c05149d21cee16f
DaGestan39 <artemzaporozec97@gmail.com>
2026-05-04 08:02:00 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,30 @@
+import asyncio
+
+import os
+from dotenv import load_dotenv
+
+from aiogram import Bot, Dispatcher
+from aiogram.filters import Command
+from aiogram.types import Message
+
+load_dotenv()
+TOKEN = os.getenv("BOT_TOKEN")
+
+dp = Dispatcher()
+
+
+
+@dp.message(Command("start"))
+async def command_start_handler(message: Message) -> None:
+    await message.answer("Hello! I'm a bot created with aiogram.")
+
+
+
+async def main() -> None:
+    bot = Bot(token=TOKEN)
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
+          
